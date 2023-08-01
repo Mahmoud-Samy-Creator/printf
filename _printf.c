32a85f0b5e8a885cd547957ae66dadbdb2d214a4
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-01 19:53:20 +0300

    printf function

--- _printf.c
+++ _printf.c
@@ -6,6 +6,13 @@
  * Return: string
  */
 
+int nulling(char *format)
+{
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
+}
 int _printf(const char *format, ...)
 {
 	size_t i = 0;
@@ -13,10 +20,8 @@ int _printf(const char *format, ...)
 	char str[2500] = "", temp[2] = "";
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-	{
+	if (nulling(format) == -1)
 		return (-1);
-	}
 
 	for (i = 0 ; format[i] ; i++)
 	{
