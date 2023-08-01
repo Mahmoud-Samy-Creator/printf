32024dad6d4617397828ad7efac40516648221dc
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-01 18:49:29 +0300

    printf function implementation

--- _printf.c
+++ _printf.c
@@ -14,11 +14,7 @@ int _printf(const char *format, ...)
 	size_t len = 0;
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-	{
-		va_end(args);
-		return (-1);
-	}
+	special(format, args);
 
 	for (i = 0 ; format[i] ; i++)
 	{
