4b2d06e16bb68fac8c67d3e52350022cda08b4c5
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-01 20:53:57 +0300

    printf function

--- _printf.c
+++ _printf.c
@@ -5,11 +5,19 @@
  * @format: format string
  * Return: string
  */
-char *printing(char *format, va_list args)
+
+int _printf(const char *format, ...)
 {
 	size_t i = 0;
-	char *str = malloc(strlen(format));
-	char temp[2] = "";
+	va_list args;
+	char str[2500] = "", temp[2] = "";
+
+	va_start(args, format);
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
 
 	for (i = 0 ; format[i] ; i++)
 	{
@@ -44,23 +52,6 @@ char *printing(char *format, va_list args)
 			strcat(str, temp);
 		}
 	}
-	return (str);
-}
-int _printf(const char *format, ...)
-{
-	int i = 0;
-	va_list args;
-	char string[strlen(format)];
-
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-	for (i = 0 ; format[i] ; i++)
-		string[i] = format[i];
-
-	char *str = printing(string, args);
-
 	va_end(args);
 	write(1, str, strlen(str));
 	return (strlen(str));
