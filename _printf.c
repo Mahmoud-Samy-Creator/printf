a8346e5d2e439b6895434d2c908eeb92bb47edc2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-02 19:34:06 +0300

    printf function

--- _printf.c
+++ _printf.c
@@ -8,52 +8,64 @@
 
 int _printf(const char *format, ...)
 {
-	size_t i = 0;
-	va_list args;
-	char *ch = malloc(2);
-	char str[2500] = "", temp[2] = "";
+    size_t i = 0;
+    va_list args;
+    size_t len = 0; // To keep track of the total length
+    char temp[2] = "";
 
-	va_start(args, format);
+    va_start(args, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+        return (-1);
 
-	for (i = 0 ; format[i] ; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == 'c')
-			{
-				ch = print_char(args);
-				strcat(str, ch);
-				i++;
-			}
-			else if (format[i + 1] == 's')
-			{
-				strcat(str, print_str(args));
-				i++;
-			}
-			else if (format[i + 1] == '%')
-			{
-				strcat(str, "%");
-				i++;
-			}
-			else
-			{
-				temp[0] = format[i];
-				temp[1] = '\0';
-				strcat(str, temp);
-			}
-		}
-		else
-		{
-			temp[0] = format[i];
-			temp[1] = '\0';
-			strcat(str, temp);
-		}
-	}
-	va_end(args);
-	free(ch);
-	write(1, str, strlen(str));
-	return (strlen(str));
+    for (i = 0; format[i]; i++)
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == 'c')
+            {
+                char c = (char) va_arg(args, int);
+                temp[0] = c;
+                temp[1] = '\0';
+                write(1, temp, 1);
+                len += 1;
+                i++;
+            }
+            else if (format[i + 1] == 's')
+            {
+                const char *s = va_arg(args, const char *);
+                if (s != NULL)
+                {
+                    len += strlen(s);
+                    write(1, s, strlen(s)); // Print the string directly
+                }
+                i++;
+            }
+            else if (format[i + 1] == '%')
+            {
+                temp[0] = format[i];
+                temp[1] = '\0';
+                write(1, temp, 1);
+                len += 1;
+                i++;
+            }
+            else
+            {
+                temp[0] = format[i];
+                temp[1] = '\0';
+                write(1, temp, 1);
+                len += 1;
+            }
+        }
+        else
+        {
+            temp[0] = format[i];
+            temp[1] = '\0';
+            write(1, temp, 1);
+            len += 1;
+        }
+    }
+    va_end(args);
+    return (int)len; // Return the total number of characters printed
 }
+
