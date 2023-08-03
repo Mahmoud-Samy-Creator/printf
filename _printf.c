4326f57dda954e2c43332c3fdca7dd344b81f0a0
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-03 13:08:55 +0300

    Header file

--- _printf.c
+++ _printf.c
@@ -27,7 +27,7 @@ int _printf(const char *format, ...)
 			}
 			else if (format[i + 1] == '%')
 			{
-				len += print_percent (format[i + 1]);
+				len += print_percent(format[i + 1]);
 				i++;
 			}
 			else if (format[i + 1] == 's')
