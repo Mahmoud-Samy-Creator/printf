82ae039f67a0a5182f26d0e3a2719be4427b37f0
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-03 13:16:31 +0300

    functions

--- functions.c
+++ functions.c
@@ -12,7 +12,7 @@ int print_char(va_list args)
 	temp[0] = va_arg(args, int);
 	temp[1] = '\0';
 
-	write (1, temp, 1);
+	write(1, temp, 1);
 	return (1);
 }
 
@@ -26,11 +26,11 @@ int print_str(va_list args)
 	char *str = va_arg(args, char *);
 
 	if (str == NULL)
-		{
-			write(1, "(null)", 6);
-			return (6);
-		}
-	
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+
 	write(1, str, strlen(str));
 
 	return (strlen(str));
@@ -40,12 +40,12 @@ int print_str(va_list args)
  * print_statments - A function for printing a statment
  * @format: a char to print
  * Return:1
-*/
+ */
 
 int print_statments(char format)
 {
 	char temp[2] = "";
-	
+
 	temp[0] = format;
 	temp[1] = '\0';
 	write(1, temp, 1);
