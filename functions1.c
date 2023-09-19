27dfc5469b26c637d39e69c0fdea058b69f0ac41
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-19 21:18:25 +0300

    Update project

--- functions1.c
+++ functions1.c
@@ -92,3 +92,67 @@ int print_int(va_list args, int *i, int *len)
 	free(string);
 	return (1);
 }
+
+/**
+ * print_unsigned_int - A function to print integers
+ * @args: list of arguments
+ * @i: pointer to counter
+ * @len: pointer to length
+ * Return: 1
+ */
+
+unsigned long int print_unsigned_int(va_list args, int *i, int *len)
+{
+	unsigned long int integer = va_arg(args, int);
+	unsigned long int k = 0, j = 0, digit = 0, temp = integer, final = integer;
+	char *str, *string;
+
+	if (integer == 0)
+		return (print_zero(&(*i), &(*len)));
+
+	if (integer == INT_MIN)
+		return (print_max_int(&(*i), &(*len)));
+
+	if (integer < 0)
+	{
+		integer = -integer;
+		temp = -temp;
+	}
+	while (integer != 0)
+	{
+		integer /= 10;
+		digit++;
+	}
+	integer = temp;
+	str = malloc(digit + 1);
+	string = malloc(digit + 1);
+	for (k = 0 ; k < digit ; k++)
+	{
+		integer %= 10;
+		temp /= 10;
+		str[k] = integer + '0';
+		integer = temp;
+	}
+	str[k] = '\0';
+	while (k--)
+	{
+		string[j] = str[k];
+		j++;
+	}
+	string[j] = '\0';
+
+	if (final < 0)
+	{
+		write(1, "-", 1);
+		write(1, string, digit);
+		(*i)++;
+		(*len) = (*len) + 1 + digit;
+		return (1);
+	}
+	write(1, string, digit);
+	(*i)++;
+	(*len) += strlen(string);
+	free(str);
+	free(string);
+	return (1);
+}
