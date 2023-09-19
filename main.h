8e9f0c380d7ce288134f489ecca4f848f0d6df0f
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-19 21:03:00 +0300

    Update project

--- main.h
+++ main.h
@@ -15,5 +15,6 @@ int print_statments(char format, int *len);
 int print_percent(int *i, int *len);
 int print_int(va_list args, int *i, int *len);
 int print_zero(int *i, int *len);
+int print_max_int(int *i, int *len);
 
 #endif
