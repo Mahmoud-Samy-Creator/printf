b891e38a86c30d0ad8a165ec5ebe99191bcb9785
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-19 22:14:28 +0300

    Update header file with function map

--- main.h
+++ main.h
@@ -9,10 +9,14 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+
+/* functions.c */
 int print_char(va_list args, int *i, int *len);
 int print_str(va_list args, int *i, int *len);
 int print_statments(char format, int *len);
 int print_percent(int *i, int *len);
+
+/* functions1.c */
 int print_int(va_list args, int *i, int *len);
 int print_zero(int *i, int *len);
 int print_max_int(int *i, int *len);
